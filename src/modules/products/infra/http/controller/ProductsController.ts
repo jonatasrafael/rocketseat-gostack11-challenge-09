7c2b461fbda5ed4f29948684f5fd9830257c68fc
Jonatas Rafael Carneiro Ribeiro <jonatasrafael@jonatasrafael.com>
2020-08-10 21:00:11 -0300

    challenge finalized

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -2,9 +2,20 @@ import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
 import CreateProductService from '@modules/products/services/CreateProductService';
+import productsRouter from '../routes/products.routes';
 
 export default class ProductsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, price, quantity } = request.body;
+
+    const createProduct = container.resolve(CreateProductService);
+
+    const product = await createProduct.execute({
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
   }
 }
